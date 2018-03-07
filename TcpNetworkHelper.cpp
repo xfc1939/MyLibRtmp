faf39754c8c70fabb831495dc316fc118fba46b6
xfc <xfc_1939@foxmail.com>
2018-03-07 14:41:39 +0800

    第一次创建文档

---
+++ TcpNetworkHelper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by XFC on 2018/3/7.
+//
+
+#include "TcpNetworkHelper.h"
---
+++ TcpNetworkHelper.h
@@ -0,0 +1,14 @@
+//
+// Created by XFC on 2018/3/7.
+//
+
+#ifndef MYLIBRTMP_TCPNETWORKHELPER_H
+#define MYLIBRTMP_TCPNETWORKHELPER_H
+
+
+class TcpNetworkHelper {
+
+};
+
+
+#endif //MYLIBRTMP_TCPNETWORKHELPER_H
